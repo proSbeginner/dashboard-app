a78ab41cd406022a0d18036294e3cc5262f6e431
Phai Panda <prosbeginner@hotmail.com>
2025-06-28 13:03:35 +0700

    move global css to app layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import '@/app/ui/global.css'
 import Image from 'next/image';
 
 export default function Page() {
