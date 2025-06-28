f5057114870f359da5f0bc6bb4c73746892f0d26
Phai Panda <prosbeginner@hotmail.com>
2025-06-28 22:33:23 +0700

    fetching latest top 5 invoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import { lusitana } from "@/app/ui/fonts";
-import { fetchRevenue } from "../lib/data";
+import { fetchLatestInvoices, fetchRevenue } from "../lib/data";
 import RevenueChart from "../ui/dashboard/revenue-chart";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
 
 export default async function Page() {
   const revenue = await fetchRevenue()
+  const latestInvoices = await fetchLatestInvoices()
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
@@ -12,6 +14,7 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue}  />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   )
