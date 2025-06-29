0d2dfbfecad32c3502ccc198eea6cca1aeeed6c3
Phai Panda <prosbeginner@hotmail.com>
2025-06-29 14:36:41 +0700

    streaming card data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -58,6 +58,9 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
+    console.log('Fetching card data...');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
